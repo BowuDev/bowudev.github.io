fcfb0a4a35796fbf915b04af0ff7db1e7a64b4fa
Nicholas Phillips <nicophillips@live.com>
2025-03-17 06:48:08 -0400

    Added animation to dot in header and slightly update the layout.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import "./page.css";
+
 export default function Home() {
     return (
-        <div className="flex items-center justify-center min-h-screen p-8 [font-family:var(--font-geist-sans)]">
+        <div
+            className="flex flex-col items-center justify-center min-h-screen p-8 [font-family:var(--font-geist-sans)]">
             <h1 className="f-text-48-96 text-center">Bowu<span className="opacity-20">.</span>Dev</h1>
         </div>
     );
